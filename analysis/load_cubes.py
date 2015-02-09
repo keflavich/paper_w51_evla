859a3b7fd17776733d8ac2d8fd127193a331d35b
Adam Ginsburg <keflavich@yahoo.com>
2015-02-09 08:55:11 +0100

    add paths, set up ds9 for real

--- analysis/load_cubes.py
+++ analysis/load_cubes.py
@@ -6,9 +6,19 @@ import ds9
 
 dd = ds9.ds9()
 
-cubes = {name: SpectralCube(dpath(fn)) for name,fn in cube_names.items()}
+cubes = {name: SpectralCube.read(dpath(fn)) for name,fn in cube_names.items()}
 
 vcubes = {name: cube.with_spectral_unit(u.km/u.s) for name,cube in cubes.items()}
 
 for name,vc in vcubes.items():
-    vc.to_ds9(dd.....)
+    vc.to_ds9(dd.id, newframe=True)
+
+dd.set('tile yes')
+dd.set('frame frameno 1')
+dd.set('frame delete')
+dd.set('scale limits -0.1 0.1')
+dd.set('wcs fk5')
+dd.set('lock frame wcs')
+dd.set('lock slice wcs')
+dd.set('lock scale yes')
+dd.set('lock color yes')
