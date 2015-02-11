c4ecfd502cf7d7f94eb7eca9db215266e81495bd
Adam Ginsburg <keflavich@yahoo.com>
2015-02-11 18:41:59 +0100

    add a lot of new plotting material and some text

---
+++ plot_codes/centroid_north_core.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pylab as pl
+from paths import dpath,fpath,rpath
+from spectral_cube import SpectralCube
+from astropy import coordinates
+from astropy import units as u
+from astropy.io import fits
+import aplpy
+import pylab
+
+import pyregion
+import gaussfitter
+
+cube = SpectralCube.read(dpath('W51Ku_BD_h2co_v30to90_natural_contsub.image.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
+
+r = pyregion.open(rpath('W51_22_emission.reg'))
+scube = cube.subcube_from_ds9region(pyregion.ShapeList([x for x in r if 'text' in x.attr[1] and x.attr[1]['text']=='W51NorthCore']))
+
+
+gaussfits = [gaussfitter.gaussfit(
+    np.nan_to_num((scube[scube.closest_spectral_channel(v*u.km/u.s),:,:]/scube.max()).value),
+    return_error=True )  for v in np.arange(vr[0],vr[1]+0.5,0.5)]
+
+centroids = np.array([gf[0][2:4] for gf in gaussfits])
+ecentroids = np.array([gf[1][2:4] for gf in gaussfits])
+
+pl.clf()
+pl.errorbar(centroids[:,0], centroids[:,1], xerr=ecentroids[:,0], yerr=ecentroids[:,1], linestyle='none', zorder=-1)
+pl.scatter(centroids[:,0], centroids[:,1], marker='o', c=np.arange(vr[0],vr[1]+0.5,0.5), s=100)
+
