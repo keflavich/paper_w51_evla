105ab19ef3f71c9621176756d858ab7b6a3d9689
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-08-03 18:15:41 +0200

    cband diff: smoothed difference image.  The offset needs fixing.  Sad.

--- analysis/cband_diff.py
+++ analysis/cband_diff.py
@@ -5,6 +5,7 @@ import reproject
 import paths
 import image_registration
 from radio_beam import Beam
+from astropy import convolution
 
 epoch1 = fits.open(paths.dpath("W51-CBAND-feathered.fits"))
 epoch1[0].data = epoch1[0].data.squeeze()
@@ -15,7 +16,8 @@ epoch1header['NAXIS2'] = epoch1[0].data.shape[0]
 epoch3 = fits.open(paths.dpath("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits"))
 beam3 = Beam.from_fits_header(epoch3[0].header)
 epoch3[0].data = epoch3[0].data.squeeze()
-epoch3header = wcs.WCS(epoch3[0].header).sub([wcs.WCSSUB_CELESTIAL]).to_header()
+wcs3 = wcs.WCS(epoch3[0].header).sub([wcs.WCSSUB_CELESTIAL])
+epoch3header = wcs3.to_header()
 epoch3header['NAXIS'] = 2
 epoch3header['NAXIS1'] = epoch3[0].data.shape[1]
 epoch3header['NAXIS2'] = epoch3[0].data.shape[0]
@@ -32,3 +34,13 @@ epoch1matched = image_registration.fft_tools.shift2d(epoch1reproj*scalefactor, -
 diff =  epoch3[0].data - epoch1matched
 diffhdu = fits.PrimaryHDU(data=diff, header=epoch3header)
 diffhdu.writeto(paths.dpath("Cband_Epoch3-Epoch1.fits"), clobber=True, output_verify='fix')
+
+smooth_beam = beam1.deconvolve(beam3)
+kernel = smooth_beam.major
+pixscale = (wcs3.pixel_scale_matrix.diagonal()**2).sum()**0.5 * u.deg
+smoothed3 = convolution.convolve_fft(epoch3[0].data,
+                                     convolution.Gaussian2DKernel(kernel/pixscale))
+
+diffsm =  smoothed3 - epoch1matched
+diffsmhdu = fits.PrimaryHDU(data=diffsm, header=epoch3header)
+diffsmhdu.writeto(paths.dpath("Cband_Epoch3sm-Epoch1.fits"), clobber=True, output_verify='fix')
