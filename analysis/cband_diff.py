a030c42b4eafb786e1e5a4017185d8f654a05c7f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-08-05 16:13:50 +0200

    added new pointsources based on the unsharp masked image and added more annotations to various figures

--- analysis/cband_diff.py
+++ analysis/cband_diff.py
@@ -1,4 +1,6 @@
 from astropy.io import fits
+from astropy import coordinates
+from astropy import units as u
 from astropy import wcs
 import FITS_tools
 import reproject
@@ -6,6 +8,7 @@ import paths
 import image_registration
 from radio_beam import Beam
 from astropy import convolution
+import gaussfitter
 
 epoch1 = fits.open(paths.dpath("W51-CBAND-feathered.fits"))
 epoch1[0].data = epoch1[0].data.squeeze()
@@ -13,6 +16,7 @@ beam1 = Beam.from_fits_header(epoch1[0].header)
 epoch1[0].header = epoch1header = wcs.WCS(epoch1[0].header).sub([wcs.WCSSUB_CELESTIAL]).to_header()
 epoch1header['NAXIS1'] = epoch1[0].data.shape[1]
 epoch1header['NAXIS2'] = epoch1[0].data.shape[0]
+wcs1 = wcs.WCS(epoch1[0].header).sub([wcs.WCSSUB_CELESTIAL])
 epoch3 = fits.open(paths.dpath("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits"))
 beam3 = Beam.from_fits_header(epoch3[0].header)
 epoch3[0].data = epoch3[0].data.squeeze()
@@ -29,7 +33,22 @@ scalefactor = (beam3.sr/beam1.sr).value
 
 xshift,yshift, ex, ey = image_registration.chi2_shift(epoch3[0].data, epoch1reproj*scalefactor, err=0.001)
 
-epoch1matched = image_registration.fft_tools.shift2d(epoch1reproj*scalefactor, -xshift, -yshift)
+center = coordinates.SkyCoord(290.92443*u.deg, 14.515755*u.deg, frame='fk5')
+xx,yy = wcs1.wcs_world2pix([[center.fk4.ra.deg, center.fk4.dec.deg]], 0)[0]
+subim1 = epoch1[0].data[yy-10:yy+10, xx-10:xx+10]
+wcs1sub = wcs1[yy-10:yy+10, xx-10:xx+10]
+xx,yy = wcs3.wcs_world2pix([[center.fk5.ra.deg, center.fk5.dec.deg]], 0)[0]
+wcs3sub = wcs3[yy-10:yy+10, xx-10:xx+10]
+subim3 = epoch3[0].data[yy-10:yy+10, xx-10:xx+10]
+gf1 = gaussfitter.gaussfit(subim1)
+gf3 = gaussfitter.gaussfit(subim3)
+
+dx1,dy1 = gf1[2:4]
+dx3,dy3 = gf3[2:4]
+dx_gf, dy_gf = dx3-dx1, dy3-dy1
+
+epoch1matched_fft = image_registration.fft_tools.shift2d(epoch1reproj*scalefactor, -xshift, -yshift)
+epoch1matched = image_registration.fft_tools.shift2d(epoch1reproj*scalefactor, dx_gf, dy_gf)
 
 diff =  epoch3[0].data - epoch1matched
 diffhdu = fits.PrimaryHDU(data=diff, header=epoch3header)
@@ -44,3 +63,17 @@ smoothed3 = convolution.convolve_fft(epoch3[0].data,
 diffsm =  smoothed3 - epoch1matched
 diffsmhdu = fits.PrimaryHDU(data=diffsm, header=epoch3header)
 diffsmhdu.writeto(paths.dpath("Cband_Epoch3sm-Epoch1.fits"), clobber=True, output_verify='fix')
+
+diff3sm =  epoch3[0].data - smoothed3
+diff3smhdu = fits.PrimaryHDU(data=diff3sm, header=epoch3header)
+diff3smhdu.writeto(paths.dpath("Cband_Epoch3sm-Epoch3.fits"), clobber=True, output_verify='fix')
+
+import pylab as pl
+import mpl_plot_templates
+pl.figure(1).clf()
+mpl_plot_templates.adaptive_param_plot(smoothed3.ravel(), epoch1matched.ravel())
+pl.plot([0,0.012], [0,0.012], 'k--', alpha=0.3)
+pl.figure(2).clf()
+mpl_plot_templates.adaptive_param_plot(smoothed3.ravel(), epoch1matched_fft.ravel())
+pl.plot([0,0.012], [0,0.012], 'k--', alpha=0.3)
+
--- plot_codes/diffuse_features.py
+++ plot_codes/diffuse_features.py
@@ -1,3 +1,4 @@
+import numpy as np
 import paths
 from astropy.io import fits
 import wcsaxes
@@ -8,18 +9,48 @@ from astropy.visualization import AsinhStretch,LinearStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 pl.matplotlib.rc_file('pubfiguresrc')
 
+annotations = {'w51main_peak': [[(290.92755, 14.511689), [-3, 3], [-20, 20], 'r'],
+                                [(290.92839, 14.510252), [-5, 0], [-30, 0], 'r'],
+                                [(290.92643, 14.512817), [-10, 0], [-35, 0], 'b'],
+                                [(290.92375, 14.511762), [-10, 0], [-40, 0], 'b'],
+                                #[(290.92358, 14.510963), [10, -10], [30, -30], 'b'],
+                                [(290.92064, 14.510635), [0, 0], [-52, 78], 'c'],
+                               ],
+               'irs2_C_low' : [[(290.91379, 14.522722), [-5, 5], [-20, 30], 'b'],
+                               [(290.92188, 14.518806), [0, 0], [30, 0], 'r'],
+                               [(290.92088, 14.516285), [0, 0], [20, 20], 'r'],
+                               [(290.91549, 14.51498), [0, 0], [-5, 25], 'r'],
+                              ],
+              }
+annotations['w51main_peak_diff'] = annotations['w51main_peak']
+annotations['irs2_C_high'] = annotations['irs2_C_low'] + [[(290.91597, 14.51945), (0, -2), (0, -30), 'c']]
+annotations['irs2_C_diff'] = annotations['irs2_C_low']
+annotations['irs2_Ku_low'] = annotations['irs2_C_low']
+
+def annotate(ax, xy, to_offset, from_offset, color, mywcs):
+
+    ann = pl.Annotation(".", xy=mywcs.wcs_world2pix([xy],0)[0]+np.array(to_offset),
+                        xytext=mywcs.wcs_world2pix([xy],0)[0]+np.array(from_offset),
+                        arrowprops=dict(arrowstyle="->", edgecolor=color,
+                                        color=color), color=color,
+                        clip_on=False)
+    ann.set_text("")
+    ax.add_artist(ann)
+
 for fn,pfx,coord_limits, (vmin,vmax), name, stretch in (
-    ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.94225, 14.505832, ), (290.93794,  14.509374, ),], (-1e-1,3e-1), 'e11_bow', LinearStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92977, 14.508804), (290.92003, 14.515556)], (0.01, 9), 'w51main_peak', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92462, 14.516962), (290.92352, 14.517891)], (2e-2, 2.0), 'e6', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.90023, 14.523703), (290.89873, 14.525156)], (2e-2, 0.3), 'd3', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.93729, 14.485868), (290.93594, 14.487230)], (-2e-2, 0.3), 'e7', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.93283, 14.506909), (290.93200, 14.507676)], (6e-2, 9), 'e1', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92248, 14.513314), (290.90971, 14.525246)], (-6e-2, 5), 'irs2_C_low', AsinhStretch()),
-    ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.92248, 14.513314), (290.90971, 14.525246)], (-6e-2, 5), 'irs2_Ku_low', AsinhStretch()),
-    ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92248, 14.513314), (290.90971, 14.525246)], (0, 13), 'irs2_C_high', AsinhStretch()),
-    ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.92248, 14.513314), (290.90971, 14.525246)], (0, 52), 'irs2_Ku_high', AsinhStretch()),
-    ("Cband_Epoch3-Epoch1.fits", 'C', [ (290.92977, 14.508804), (290.92003, 14.515556)], (-0.6, 1), 'w51main_peak_diff', LinearStretch())):
+     ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.94225, 14.505832, ), (290.93794,  14.509374, ),], (-1e-1,3e-1), 'e11_bow', LinearStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.9304, 14.5083), (290.9194, 14.5189)], (0.01, 9), 'w51main_peak', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92462, 14.516962), (290.92352, 14.517891)], (2e-2, 2.0), 'e6', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.90023, 14.523703), (290.89873, 14.525156)], (2e-2, 0.3), 'd3', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.93729, 14.485868), (290.93594, 14.487230)], (-2e-2, 0.3), 'e7', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.93283, 14.506909), (290.93200, 14.507676)], (6e-2, 9), 'e1', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92402, 14.513314), (290.90971, 14.525246)], (-6e-2, 5), 'irs2_C_low', AsinhStretch()),
+     ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.92402, 14.513314), (290.90971, 14.525246)], (-6e-2, 5), 'irs2_Ku_low', AsinhStretch()),
+     ("W51C_ACarray_continuum_4096_both_uniform_contsplit.clean.image.fits", 'C', [ (290.92402, 14.513314), (290.90971, 14.525246)], (0, 13), 'irs2_C_high', AsinhStretch()),
+     ("W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits", 'Ku', [ (290.92402, 14.513314), (290.90971, 14.525246)], (0, 52), 'irs2_Ku_high', AsinhStretch()),
+     ("Cband_Epoch3sm-Epoch3.fits", 'C', [ (290.9304, 14.5083), (290.9194, 14.5189)], (-0.2, 1), 'w51main_peak_diff', LinearStretch()),
+     ("Cband_Epoch3sm-Epoch3.fits", 'C', [ (290.92402, 14.513314), (290.90971, 14.525246)], (-0.2, 1.0), 'irs2_C_diff', LinearStretch()),
+    ):
 
     hdu = fits.open(paths.dpath(fn))[0]
     mywcs = wcs.WCS(hdu.header).sub([wcs.WCSSUB_CELESTIAL])
@@ -34,7 +65,7 @@ for fn,pfx,coord_limits, (vmin,vmax), name, stretch in (
                    vmin=vmin, vmax=vmax,
                    norm=ImageNormalize(stretch=stretch))
 
-    (x1,y1),(x2,y2) = mywcs.wcs_world2pix( coord_limits, 0)
+    (x1,y1),(x2,y2) = mywcs.wcs_world2pix(coord_limits, 0)
     ax.set_xlim(x1,x2)
     ax.set_ylim(y1,y2)
     ra = ax.coords['ra']
@@ -44,6 +75,10 @@ for fn,pfx,coord_limits, (vmin,vmax), name, stretch in (
     dec.set_axislabel("Dec")
     cb = fig.colorbar(im)
     cb.set_label("mJy/beam")
+
+    if name in annotations:
+        for xy, to_offset, from_offset, color in annotations[name]:
+            annotate(ax, xy, to_offset, from_offset, color, mywcs)
 
     pl.draw(); pl.show()
     pl.savefig(paths.fpath("diffuse/{0}.png".format(name)), bbox_inches='tight')
