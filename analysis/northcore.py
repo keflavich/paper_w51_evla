295cd71e012effee6c141df6218a18b9487b258d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-08-13 12:25:45 +0200

    North Core analysis

---
+++ analysis/northcore.py
@@ -0,0 +1,111 @@
+"""
+Simple analysis of the north core
+"""
+import numpy as np
+from astropy import units as u
+from astropy import constants
+import pyradex
+
+integrated_intensity = 109*u.mJy # from centroid_north_core
+line_integral = 644*u.K*u.km/u.s / 5 # 5 beams
+radius = 0.024*u.pc
+
+density = 1e6 * u.cm**-3
+# assume it is a spherical gaussian, so sqrt(2 pi)^3
+mass_n1e6 = density * ((2*np.pi)**0.5*radius)**3 * (2.8*u.Da)
+print("Mass assuming n={0}: {1}".format(density, mass_n1e6.to(u.M_sun)))
+
+# column density computation based on Mangum & Shirley 2015
+dipole_moment = 2.331*u.Debye*(1e-18*u.esu/u.Debye)
+# 1 Debye = 1e-18 esu
+
+def Jnu(nu, T):
+    return constants.h*nu/constants.k_B / (np.exp(constants.h*nu/(constants.k_B*T))-1)
+R = pyradex.Radex(species='oh2co-h2', density=1e3, temperature=20, column=1e12)
+def Qrot(temperature, R=R):
+    return R.partition_function(temperature)
+
+# def Qrot(temperature):
+#     A0 = 281970.58
+#     B0 = 38833.987
+#     C0 = 34004.244
+#     m = B0**2/A0/C0 
+#     what is sigma?
+#     term1 = (m*np.pi)**0.5 / sigma
+#     term2 = np.exp(constants.h*B0*(4-m)/(12*constants.k_B*temperature))
+#     term3 = (constants.k_B*temperature/(constants.h*B0))**(3/2.)
+#     term4 = (1+1/90. * (h*B_0*(1-m)/(constants.k_B*temperature))**2)
+#     return term1*term2*term3*term4
+
+def Nthintot_(nu, line_integral, degeneracy_u, dipole_moment, temperature, Eu, Tex, Tbg=2.7315*u.K):
+    term1 = (3*constants.h/(8*np.pi**3 * dipole_moment**2))
+    term2 = Qrot(temperature)/degeneracy_u 
+    term3 = np.exp(Eu/(constants.k_B * Tex)) / (np.exp(constants.h*nu/(constants.k_B * Tex)) - 1)
+    term4 = 1./(Jnu(nu,Tex)-Jnu(nu,Tbg))
+    term5 = line_integral.to(u.K*u.km/u.s)
+    return term1 * term2 * term3 * term4 * term5 * u.cm**-2
+
+def Nthintot(nu, line_integral, degeneracy_u, dipole_moment, temperature, Eu, Tex, Ju=2, K=1, Tbg=2.7315*u.K):
+    Ri = 1
+    term1 = (3*constants.h/(8*np.pi**3 * dipole_moment**2 * Ri))
+    term1a = (Ju*(Ju+1))/K**2
+    gJ = 2*Ju+1
+    gK = 2
+    gI = 3/4.
+    term2 = Qrot(temperature) / (gJ*gK*gI)
+    term3 = np.exp(Eu/(constants.k_B * Tex)) / (np.exp(constants.h*nu/(constants.k_B * Tex)) - 1)
+    term4 = 1./(Jnu(nu,Tex)-Jnu(nu,Tbg))
+    term5 = line_integral.to(u.K*u.km/u.s)
+    return term1 * term1a * term2 * term3 * term4 * term5 * u.cm**-2
+
+temperature = Tex = 25*u.K
+
+column = Nthintot(nu=R.frequency[2], line_integral=line_integral,
+                  degeneracy_u=R.statistical_weight[2],
+                  dipole_moment=dipole_moment, temperature=temperature,
+                  Eu=R.upperstateenergy[2]*u.K*constants.k_B,
+                  Tex=Tex,).to(u.cm**-2)
+
+print("Column density assuming LTE and T=Tex={0}: {1}".format(temperature, column))
+
+temperature = Tex = 50*u.K
+column = Nthintot(nu=R.frequency[2], line_integral=line_integral,
+                  degeneracy_u=R.statistical_weight[2],
+                  dipole_moment=dipole_moment, temperature=temperature,
+                  Eu=R.upperstateenergy[2]*u.K*constants.k_B,
+                  Tex=Tex,).to(u.cm**-2)
+print("Column density assuming LTE and T=Tex={0}: {1}".format(temperature, column))
+
+temperature = 20*u.K
+Tex = 20*u.K
+
+column = Nthintot(nu=R.frequency[2], line_integral=line_integral,
+                  degeneracy_u=R.statistical_weight[2],
+                  dipole_moment=dipole_moment, temperature=temperature,
+                  Eu=R.upperstateenergy[2]*u.K*constants.k_B,
+                  Tex=Tex,).to(u.cm**-2)
+print("Column density assuming LTE and T={2} and Tex={0}: {1}".format(temperature, column, temperature))
+
+temperature = 100*u.K
+Tex = 100*u.K
+
+column = Nthintot(nu=R.frequency[2], line_integral=line_integral,
+                  degeneracy_u=R.statistical_weight[2],
+                  dipole_moment=dipole_moment, temperature=temperature,
+                  Eu=R.upperstateenergy[2]*u.K*constants.k_B,
+                  Tex=Tex,).to(u.cm**-2)
+print("Column density assuming LTE and T={2} and Tex={0}: {1}".format(temperature, column, temperature))
+
+temperature = 200*u.K
+Tex = 200*u.K
+
+column = Nthintot(nu=R.frequency[2], line_integral=line_integral,
+                  degeneracy_u=R.statistical_weight[2],
+                  dipole_moment=dipole_moment, temperature=temperature,
+                  Eu=R.upperstateenergy[2]*u.K*constants.k_B,
+                  Tex=Tex,).to(u.cm**-2)
+print("Column density assuming LTE and T={2} and Tex={0}: {1}".format(temperature, column, temperature))
+
+Xh2co = 1e-9
+mass_col = column * ((2*np.pi)**0.5 * radius)**2 * 2.8*u.Da / Xh2co
+print("Mass given column, assuming X={Xh2co}: {0}".format(mass_col.to(u.M_sun), Xh2co=Xh2co, ))
--- plot_codes/centroid_north_core.py
+++ plot_codes/centroid_north_core.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pylab as pl
+import common_constants
 from paths import dpath,fpath,rpath
 from spectral_cube import SpectralCube
 from astropy.utils.console import ProgressBar
@@ -12,8 +13,10 @@ import pylab
 
 import pyregion
 import gaussfitter
+import radio_beam
 
 cube = SpectralCube.read(dpath('W51Ku_BD_h2co_v30to90_natural_contsub.image.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
+beam = radio_beam.Beam.from_fits_header(cube.header)
 
 r = pyregion.open(rpath('W51_22_emission.reg'))
 scube = cube.subcube_from_ds9region(pyregion.ShapeList([x for x in r if 'text' in x.attr[1] and x.attr[1]['text']=='NorthCore']))
@@ -21,19 +24,40 @@ pixscale = scube.wcs.pixel_scale_matrix[1,1]*u.deg
 pixscale_as = pixscale.to(u.arcsec)
 
 cubemax = scube.max() # normalize everything
-noise = (scube.spectral_slab(30*u.km/u.s, 50*u.km/u.s).std(axis=0) / cubemax).value
-noise[np.isnan(noise)] = 1e10
+noise = scube.spectral_slab(30*u.km/u.s, 50*u.km/u.s).std(axis=0)
+noise[np.isnan(noise)] = 1e10*noise.unit
+noise_norm = (noise / cubemax).value
 
+dv = cube.spectral_axis.diff().mean()
 vr = [56,63.5]
 gaussfits = [gaussfitter.gaussfit(
     np.nan_to_num((scube[scube.closest_spectral_channel(v*u.km/u.s),:,:]/cubemax).value),
-    err=noise,
-    return_error=True )  for v in ProgressBar(np.arange(vr[0],vr[1]+0.5,0.5))]
+    err=noise_norm,
+    return_error=True )  for v in ProgressBar(np.arange(vr[0],vr[1]+dv.value,dv.value))]
 
-gaussfit_total = gaussfitter.gaussfit(np.nan_to_num((scube.spectral_slab(vr[0]*u.km/u.s, vr[1]*u.km/u.s).mean(axis=0)/cubemax).value))
+integrated_image = (scube.spectral_slab(vr[0]*u.km/u.s, vr[1]*u.km/u.s).sum(axis=0))
+gaussfit_total,fitimage = gaussfitter.gaussfit(np.nan_to_num(integrated_image.value),
+                                               err=noise.value,
+                                               returnfitimage=True
+                                     )
+# sanity check: make sure the fit is OK (it is)
+pl.figure(2).clf()
+pl.imshow(integrated_image.value, cmap=pl.cm.bone_r)
+pl.contour(fitimage, cmap=pl.cm.spectral)
+
+print("Integrated gaussfit: ", gaussfit_total)
 centerx, centery = gaussfit_total[2:4]
 center_coord = coordinates.SkyCoord(*scube.wcs.sub([wcs.WCSSUB_CELESTIAL]).wcs_pix2world([[centerx, centery]], 0)[0], unit=('deg','deg'), frame='fk5')
 print("Center position: {0}".format(center_coord.to_string('hmsdms')))
+integ_intens = 1.*u.Jy/beam * gaussfit_total[1] * (gaussfit_total[4]*pixscale_as * gaussfit_total[5]*pixscale_as * 2 * np.pi)
+nbeams = ((gaussfit_total[4]*pixscale_as * gaussfit_total[5]*pixscale_as * 2 * np.pi) / beam).decompose()
+print("Integrated intensity: {0}".format(integ_intens.to(u.mJy)*dv))
+print("Integrated brightness: {0}".format(integ_intens.to(u.K, u.brightness_temperature(beam, 14.488*u.GHz))*dv))
+print("Integrated average brightness: {0}".format(integ_intens.to(u.K, u.brightness_temperature(beam, 14.488*u.GHz))*dv/nbeams))
+radius = (gaussfit_total[4]*pixscale_as * gaussfit_total[5]*pixscale_as)**0.5
+print("radius: {0} = {1}".format(radius,
+                                 (radius*common_constants.distance).to(u.pc,
+                                                                       u.dimensionless_angles())))
 
 centroids = np.array([gf[0][2:4] for gf in gaussfits])
 ecentroids = np.array([gf[1][2:4] for gf in gaussfits])
@@ -43,18 +67,18 @@ fig.clf()
 ax = fig.gca()
 sc = ax.scatter((centroids[:,0]-centerx)*pixscale_as.value,
                 (centroids[:,1]-centery)*pixscale_as.value, marker='o',
-                c=np.arange(vr[0],vr[1]+0.5,0.5), s=100, edgecolor='none',
+                c=np.arange(vr[0],vr[1]+dv.value,dv.value), s=100, edgecolor='none',
                 cmap=pl.cm.spectral)
 
 # sc doesn't have facecolors until it is drawn
 pl.draw()
 itr = zip(centroids[:,0], centroids[:,1], ecentroids[:,0], ecentroids[:,1], sc.get_facecolors())
 
-print itr
-print
+#print itr
+#print
 
 for x,y,ex,ey,c in itr:
-    print x,y,ex,ey,c
+    #print x,y,ex,ey,c
     ax.errorbar((x-centerx)*pixscale_as.value,
                 (y-centery)*pixscale_as.value,
                 xerr=ex*pixscale_as.value,
